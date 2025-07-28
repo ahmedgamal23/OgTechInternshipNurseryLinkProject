8fec38aef21ace8a152c35f53136c2157d461635
ahmed_gamal <am5679456@gmail.com>
2025-07-28 13:52:57 +0300

    add controller

--- NurseryLinkBackend/NurseryLinkProject/NurseryLinkProject.API/obj/Debug/net9.0/NurseryLinkProject.API.AssemblyInfo.cs
+++ NurseryLinkBackend/NurseryLinkProject/NurseryLinkProject.API/obj/Debug/net9.0/NurseryLinkProject.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NurseryLinkProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcb0760893125638036e2a09ea77d111a41edad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+831437e1f8ec568c4a42c812499fcdb7af2996a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NurseryLinkProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NurseryLinkProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
